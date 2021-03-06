152893e9f3e09f2570db8ffce2afc45a0ca7bafa
vcu-pstojanovic <78378662+vcu-pstojanovic@users.noreply.github.com>
2021-03-06 12:08:29 -0500

    Update homework.py
    test find_least

--- fun/homework.py
+++ fun/homework.py
@@ -14,7 +14,8 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    return None
+    test=1
+    return test
 
 
 def add_list_numbers(incoming_list):
