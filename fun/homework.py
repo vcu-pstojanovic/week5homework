c4072c38d9519ca73393ed94a3207d655b6d697f
vcu-pstojanovic <78378662+vcu-pstojanovic@users.noreply.github.com>
2021-03-06 12:19:01 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -23,7 +23,7 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-    return 0
+    return 10
 
 
 def longest_value_key(incoming_dict):
